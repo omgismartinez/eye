8ba7632a64b16585b7b10fa19c8089fcc82b21f4
omgismartinez <martnzomg@gmail.com>
2023-06-20 15:55:43 -0600

    🌌 refactor (navbar): background color

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,14 +2,14 @@ import { ChevronLeft } from 'lucide-react'
 
 export default function Navbar() {
     return (
-        <nav className='fixed inset-x-0 py-4 z-50 border-b border-_gray-border'>
+        <nav className='fixed inset-x-0 z-50 border-b bg-_white/50 backdrop-blur-sm border-_gray-border'>
             <div className='flex max-w-7xl mx-auto px-4'>
                 <div>
-                    <div className='w-[269px]'>
+                    <div className='w-[268px] py-4 border-r border-_gray-border'>
                         <div className='bg-_main w-10 h-10 rounded-full' />
                     </div>
                 </div>
-                <div className='flex items-center px-6'>
+                <div className='flex items-center px-6 py-4 w-full'>
                     <div className='bg-_gray-F7F7F7 rounded-full p-1'>
                         <ChevronLeft size={18} />
                     </div>
