3ab521b23e52a9a37cfd8aa2f8df9511034ba13f
omgismartinez <martnzomg@gmail.com>
2023-06-21 23:49:09 -0600

    🌌 refactor (navbar): return action

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import { ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
+    const pathname = usePathname()
     return (
         <nav className='fixed inset-x-0 z-50 border-b bg-_white/50 backdrop-blur-sm border-_gray-border'>
             <div className='flex max-w-7xl mx-auto px-4'>
@@ -9,12 +14,14 @@ export default function Navbar() {
                         <div className='bg-_main w-10 h-10 rounded-full' />
                     </div>
                 </div>
-                <div className='flex items-center px-6 py-4 w-full'>
-                    <div className='bg-_gray-F7F7F7 rounded-full p-1'>
-                        <ChevronLeft size={18} />
+                {pathname !== '/' && (
+                    <div className='flex items-center px-6 py-4 w-full'>
+                        <Link href='/' className='bg-_gray-F7F7F7 rounded-full p-1'>
+                            <ChevronLeft size={18} />
+                        </Link>
+                        <h2 className='text-_main font-bold text-base ml-6 whitespace-nowrap'>Nuevo Diagnóstico</h2>
                     </div>
-                    <h2 className='text-_main font-bold text-base ml-6 whitespace-nowrap'>Nuevo Diagnóstico</h2>
-                </div>
+                )}
             </div>
         </nav>
     )
