dca4c3a7e394a5ccaa182d79e45dbb44192f39ec
omgismartinez <martnzomg@gmail.com>
2023-06-22 22:29:12 -0600

    🌌 refactor (header): added className prop

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,14 @@
+import { cn } from '@/lib/utils'
+
 interface HeaderProps {
     title: string
     subtitle: string | JSX.Element
+    className?: string
 }
 
-export default function Header({ title, subtitle }: HeaderProps) {
+export default function Header({ title, subtitle, className }: HeaderProps) {
     return (
-        <div className='flex flex-col gap-6 mb-6'>
+        <div className={cn('flex flex-col gap-6 mb-6', className)}>
             <h1 className='text-2xl font-bold text-center'>{title}</h1>
             <p className='text-_gray-808080 text-sm text-center'>{subtitle}</p>
         </div>
