dbc39574fa2fa47d756bedc93b0d0653261d4f66
omgismartinez <martnzomg@gmail.com>
2023-06-23 23:01:52 -0600

    🌌 refactor (home): init

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import Header from '@/components/header'
 
 export default function Home() {
   return (
-    <main className='px-4 py-2'>
-      <div className='max-w-xl mx-auto'>
-        <section className='my-6'>
-          <Header
-            title='Retinopatía Diabética'
-            subtitle={`La retinopatía diabética es una complicación de la diabetes que afecta a los ojos.
+    <main>
+      <section className='max-w-xl mx-auto'>
+        <Header
+          title='Retinopatía Diabética'
+          subtitle={`La retinopatía diabética es una complicación de la diabetes que afecta a los ojos.
               Es causada por daño a los vasos sanguíneos de la retina.`}
-          />
-        </section>
-      </div>
+        />
+      </section>
     </main>
   )
 }
