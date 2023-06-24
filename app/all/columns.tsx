6b4de1fc9ca707c0c3f7e12bb963a6dd9949b330
omgismartinez <martnzomg@gmail.com>
2023-06-23 22:59:00 -0600

    🚀 feat: columns

---
+++ app/all/columns.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { ColumnDef } from '@tanstack/react-table'
+import { ArrowUpDown } from 'lucide-react'
+
+export type Diagnostic = {
+    id: string
+    patient: string
+    prediction: 'No dr' | 'Leve' | 'Moderado' | 'Severo' | 'Proliferativo'
+    date: string
+    phone: string
+    email: string
+}
+
+export const columns: ColumnDef<Diagnostic>[] = [
+    {
+        id: 'select',
+        header: ({ table }) => (
+            <Checkbox
+                checked={table.getIsAllPageRowsSelected()}
+                onCheckedChange={(value: any) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label='Select all'
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value: any) => row.toggleSelected(!!value)}
+                aria-label='Select row'
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: 'patient',
+        header({ column }) {
+            return (
+                <Button
+                    size={'sm'}
+                    variant={'ghost'}
+                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+                >
+                    Paciente
+                    <ArrowUpDown className='ml-2 h-4 w-4' />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: 'prediction',
+        header: 'Predicción',
+    },
+    {
+        accessorKey: 'date',
+        header({ column }) {
+            return (
+                <Button
+                    size={'sm'}
+                    variant={'ghost'}
+                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+                >
+                    Fecha
+                    <ArrowUpDown className='ml-2 h-4 w-4' />
+                </Button>
+            )
+        }
+    },
+    {
+        accessorKey: 'phone',
+        header({ column }) {
+            return (
+                <Button
+                    size={'sm'}
+                    variant={'ghost'}
+                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+                >
+                    Teléfono
+                    <ArrowUpDown className='ml-2 h-4 w-4' />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: 'email',
+        header({ column }) {
+            return (
+                <Button
+                    size={'sm'}
+                    variant={'ghost'}
+                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+                >
+                    Correo electrónico
+                    <ArrowUpDown className='ml-2 h-4 w-4' />
+                </Button>
+            )
+        }
+    },
+]
