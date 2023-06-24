7da7d83ea203621e495d36f0cff6b3ba42cde004
omgismartinez <martnzomg@gmail.com>
2023-06-23 23:03:33 -0600

    🚀 feat: all page

---
+++ app/all/page.tsx
@@ -0,0 +1,60 @@
+import Header from '@/components/header'
+import { DataTable } from './data-table'
+import { Diagnostic, columns } from './columns'
+
+async function getData(): Promise<Diagnostic[]> {
+    // Fetch data from your API here.
+    return [
+        {
+            id: "728ed52f",
+            patient: "Alvaro Martinez Martinez",
+            prediction: "No dr",
+            date: "5/6/2023, 4:41:24 p. m.",
+            phone: "1234567890",
+            email: "martinez@example.com",
+        },
+        {
+            id: "728ed52f",
+            patient: "Eduardo Perez",
+            prediction: "Leve",
+            date: "16/8/2020, 4:41:24 p. m.",
+            phone: "9876543210",
+            email: "eduperez@example.com",
+        },
+        {
+            id: "728ed52f",
+            patient: "Miriam Rodriguez",
+            prediction: "Moderado",
+            date: "12/3/2021, 4:41:24 p. m.",
+            phone: "7284612340",
+            email: "mirod@example.com",
+        },
+        {
+            id: "728ed52f",
+            patient: "Juan Perez",
+            prediction: "Severo",
+            date: "01/3/2021, 4:41:24 p. m.",
+            phone: "1120949444",
+            email: "jperez@example.com",
+        }
+        // ...
+    ]
+}
+
+export default async function All() {
+    const data = await getData()
+    return (
+        <main>
+            <Header
+                className='max-w-2xl mx-auto'
+                title='Diagnosticos realizados'
+                subtitle={<>
+                    La tabla contiene información detallada sobre cada diagnóstico realizado.
+                    Lo que permite un <strong>seguimiento preciso</strong> de la evolución de la retinopatía diabética en cada paciente.
+                </>}
+            />
+            <div className='border border-_gray-border' />
+            <DataTable columns={columns} data={data} />
+        </main>
+    )
+}
