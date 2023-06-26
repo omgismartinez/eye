735bc73b9924efafe98430321b0d42bf57fad82b
omgismartinez <martnzomg@gmail.com>
2023-06-26 15:35:37 -0600

    🚀 feat: table data

---
+++ app/all/data/data.tsx
@@ -0,0 +1,22 @@
+export const predictions = [
+    {
+        label: 'No dr',
+        value: 'no dr',
+    },
+    {
+        label: 'Leve',
+        value: 'leve',
+    },
+    {
+        label: 'Moderado',
+        value: 'moderado',
+    },
+    {
+        label: 'Severo',
+        value: 'severo',
+    },
+    {
+        label: 'Proliferativo',
+        value: 'proliferativo',
+    },
+]
