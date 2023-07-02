2cb2024b2cf54656fff2c7160dced7a8a2d0159f
omgismartinez <martnzomg@gmail.com>
2023-07-02 17:18:17 -0600

    🌌 refactor (navbar): max width + title route

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,22 +6,31 @@ import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
     const pathname = usePathname()
+
+    const routes = {
+        '/new': 'Nuevo Diagnóstico',
+        '/all': 'Todos los Diagnósticos',
+        '/patients': 'Administrar Pacientes',
+        '/terms': 'Términos y Condiciones',
+        '/account': 'Mi Cuenta',
+    }
+
     return (
         <nav className='fixed inset-x-0 z-50 border-b bg-_white/50 backdrop-blur-sm border-_gray-border'>
-            <div className='flex max-w-7xl mx-auto px-4'>
+            <div className='flex max-w-screen-2xl mx-auto'>
                 <div>
-                    <div className='w-[268px] py-4 border-r border-_gray-border'>
+                    <div className='lg:w-72 p-4 lg:border-r border-_gray-border'>
                         <div className='bg-_main w-10 h-10 rounded-full' />
                     </div>
                 </div>
-                {pathname !== '/' && (
-                    <div className='flex items-center px-6 py-4 w-full'>
-                        <Link href='/' className='bg-_gray-F7F7F7 rounded-full p-1'>
-                            <ChevronLeft size={18} />
-                        </Link>
-                        <h2 className='text-_main font-bold text-base ml-6 whitespace-nowrap'>Nuevo Diagnóstico</h2>
-                    </div>
-                )}
+                <div className='flex items-center lg:px-6 py-4 w-full'>
+                    <Link href='/' className='bg-_gray-F7F7F7 hidden lg:block rounded-full p-1'>
+                        <ChevronLeft size={18} />
+                    </Link>
+                    <h2 className='text-_main font-bold text-base lg:ml-6 whitespace-nowrap'>
+                        {routes[pathname as keyof typeof routes]}
+                    </h2>
+                </div>
             </div>
         </nav>
     )
