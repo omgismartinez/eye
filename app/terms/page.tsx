07c8d8ff534f2cf0e5c343e73b08d7239bc64228
omgismartinez <martnzomg@gmail.com>
2023-07-01 23:07:09 -0600

    🚀 feat: terms page

---
+++ app/terms/page.tsx
@@ -0,0 +1,202 @@
+export default function Terms() {
+    return (
+        <main>
+            <div className='flex justify-center items-center h-96'>
+                <h1 className='text-5xl font-bold text-center text-_main'>
+                    Términos del Servicio
+                </h1>
+            </div>
+            <div className='max-w-3xl mx-auto'>
+                <section id='introduccion'>
+                    <small className='uppercase'>
+                        <strong>Importante. </strong>
+                        Antes de utilizar nuestra aplicación, te solicitamos que leas atentamente los siguientes términos de
+                        servicio, ya que son el contrato legal entre tú y nosotros. Al utilizar nuestra aplicación, aceptas
+                        cumplir con estos términos de servicio. Si no estás de acuerdo con alguno de estos términos, te
+                        solicitamos que no utilices la aplicación.
+                    </small>
+                </section>
+                <section id='uso-de-la-aplicacion'>
+                    <div className='my-4'>
+                        <small>
+                            <strong>1. Uso de la Aplicación.</strong>
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>1.1. Uso permitido. </strong>
+                            La aplicación está diseñada exclusivamente para el propósito de detección temprana de retinopatía
+                            diabética. Queda prohibido utilizar la aplicación con fines ilegales, inapropiados, maliciosos o que infrinjan los
+                            derechos de terceros.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>1.2. Cuenta de usuario. </strong>
+                            Al utilizar nuestra aplicación, puedes crear una cuenta de usuario. Eres responsable de mantener la confidencialidad
+                            de tu cuenta y de todas las actividades realizadas bajo tu cuenta. Si tienes razones para creer que tu cuenta ha
+                            sido comprometida o utilizada de manera no autorizada, debes notificarnos de inmediato.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>1.3. Información del usuario. </strong>
+                            Al utilizar la aplicación, nos proporcionarás información personal como parte del proceso de registro y uso de la
+                            aplicación. Nos comprometemos a proteger tu privacidad y a utilizar tus datos de acuerdo con nuestra política de
+                            privacidad.
+                        </small>
+                    </div>
+                </section>
+                <section id='propiedad-intelectual'>
+                    <div className='my-4'>
+                        <small>
+                            <strong>2. Propiedad Intelectual.</strong>
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>2.1. Derechos de propiedad. </strong>
+                            Todos los derechos de propiedad intelectual relacionados con la aplicación, incluidos los derechos de
+                            autor, marcas registradas, secretos comerciales y patentes, son propiedad nuestra o de nuestros
+                            licenciantes. No se te otorga ningún derecho o licencia sobre dicha propiedad intelectual, a menos
+                            que se especifique lo contrario en estos términos de servicio.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>2.2. Uso permitido. </strong>
+                            Se te concede una licencia personal, no exclusiva, no transferible y revocable para utilizar la
+                            aplicación únicamente de acuerdo con estos términos de servicio.
+                        </small>
+                    </div>
+                </section>
+                <section id='responsabilidades-del-usuario'>
+                    <div className='my-4'>
+                        <small>
+                            <strong>3. Responsabilidades del Usuario. </strong>
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>3.1. Exactitud de la Información. </strong>
+                            Eres responsable de la exactitud y veracidad de la información que proporciones al utilizar la aplicación.
+                            Debes garantizar que todos los datos proporcionados sean precisos, actualizados y completos.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>3.2. Uso adecuado. </strong>
+                            Te comprometes a utilizar la aplicación de manera adecuada y a cumplir con todas las leyes, regulaciones
+                            y normativas aplicables al utilizar la aplicación.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>3.3. No interferencia. </strong>
+                            No debes interferir ni intentar interferir con el funcionamiento normal de la aplicación, ni realizar
+                            acciones que puedan causar daño o sobrecarga a nuestra infraestructura.
+                        </small>
+                    </div>
+                </section>
+                <section id='privacidad-y-seguridad-de-los-datos'>
+                    <div className='my-4'>
+                        <small>
+                            <strong>4. Privacidad y Seguridad de los Datos.</strong>
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>4.1. Política de privacidad. </strong>
+                            Nos comprometemos a proteger tu privacidad y la confidencialidad de tus datos. La recopilación,
+                            uso y divulgación de tus datos están sujetos a nuestra política de privacidad.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>4.2. Seguridad de la información. </strong>
+                            Implementamos medidas de seguridad para proteger tus datos y asegurar el funcionamiento seguro
+                            de la aplicación. Sin embargo, ninguna transmisión de datos por Internet o sistema de almacenamiento
+                            puede garantizarse como completamente seguro. Utilizas la aplicación bajo tu propio riesgo.
+                        </small>
+                    </div>
+                </section>
+                <section id='limitacion-de-responsabilidad'>
+                    <div className='my-4'>
+                        <small>
+                            <strong>5. Limitación de Responsabilidad.</strong>
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>5.1. Uso de la aplicación. </strong>
+                            La aplicación se proporciona "tal cual" y "según disponibilidad". No garantizamos que la aplicación sea
+                            libre de errores o que funcione de manera ininterrumpida. No nos hacemos responsables de cualquier daño
+                            directo, indirecto, incidental, consecuente o especial que puedas sufrir como resultado del uso de la
+                            aplicación.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>5.2. Enlaces externos. </strong>
+                            La aplicación puede contener enlaces a sitios web o recursos de terceros. No tenemos control sobre
+                            dichos sitios web o recursos y no nos hacemos responsables de su disponibilidad, contenido, publicidad
+                            o cualquier otro aspecto relacionado.
+                        </small>
+                    </div>
+                </section>
+                <section id='modificaciones-y-terminacion'>
+                    <div className='my-4'>
+                        <small>
+                            <strong>6. Modificaciones y Terminación. </strong>
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>6.1. Modificaciones. </strong>
+                            Nos reservamos el derecho de modificar, suspender o descontinuar la aplicación, así como de actualizar estos
+                            términos de servicio en cualquier momento. Te recomendamos revisar periódicamente los términos de servicio
+                            actualizados.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>6.2. Terminación. </strong>
+                            Podemos terminar tu acceso a la aplicación si incumples alguno de los términos de servicio establecidos aquí.
+                            En caso de terminación, deberás dejar de utilizar la aplicación y eliminar todos los materiales descargados
+                            o obtenidos de ella.
+                        </small>
+                    </div>
+                </section>
+                <section id='ley-aplicable-y-jurisdiccion'>
+                    <div className='my-4'>
+                        <small>
+                            <strong>7. Ley Aplicable y Jurisdicción. </strong>
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>7.1. Ley Aplicable. </strong>
+                            Estos términos de servicio se regirán e interpretarán de acuerdo con las leyes vigentes en el lugar de
+                            jurisdicción correspondiente.
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                            <strong>7.2. Resolución de Disputas. </strong>
+                            Cualquier disputa que surja de o en relación con estos términos de servicio se someterá a la
+                            jurisdicción exclusiva de los tribunales competentes.
+                        </small>
+                    </div>
+                </section>
+                <section id='contactanos'>
+                    <div className='my-4'>
+                        <small>
+                            Si tienes alguna pregunta o inquietud sobre estos términos de servicio, te invitamos a comunicarte
+                            con nosotros para obtener más información y aclaraciones.
+                        </small>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
+}
