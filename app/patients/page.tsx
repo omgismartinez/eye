213fcae27a1ebd6f8721806546bb26f0c59cc580
omgismartinez <martnzomg@gmail.com>
2023-07-02 17:10:50 -0600

    🌌 refactor (patients): max width

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -18,7 +18,7 @@ async function getData(): Promise<Patient[]> {
 export default async function Patients() {
     const data = await getData()
     return (
-        <main>
+        <main className='max-w-4xl mx-auto'>
             <Header
                 className='max-w-xl mx-auto'
                 title='Pacientes registrados'
