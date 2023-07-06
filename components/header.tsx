146a41b00f163ce4cdec26fc7eff90e5609aa7cc
omgismartinez <martnzomg@gmail.com>
2023-07-05 21:06:06 -0600

    🌌 refactor (header): letter-spacing title

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 export default function Header({ title, subtitle, className }: HeaderProps) {
     return (
         <div className={cn('flex flex-col gap-6 mb-6', className)}>
-            <h1 className='text-2xl font-bold text-center'>{title}</h1>
+            <h1 className='text-2xl font-bold text-center tracking-tight'>{title}</h1>
             <p className='text-_gray-808080 text-sm text-center'>{subtitle}</p>
         </div>
     )
