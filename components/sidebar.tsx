048cdcadc5dde7638f591e5e7921e2cc3008e7d4
omgismartinez <martnzomg@gmail.com>
2023-07-07 12:10:05 -0600

    🌌 refactor (sidebar): padding buttons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link'
 import clsx from 'clsx'
 import { usePathname } from 'next/navigation'
+import { Separator } from './ui/separator'
 
 export default function Sidebar() {
     const pathname = usePathname()
@@ -35,29 +36,29 @@ export default function Sidebar() {
                 <section className='flex flex-col justify-between gap-1 min-h-[650px]'>
                     <div className='flex flex-col gap-2 text-_gray-808080 text-sm font-bold'>
                         <div className='flex flex-col gap-4'>
-                            <button className='flex items-center gap-4 py-3 whitespace-nowrap rounded-lg text-_main hover:text-_main/80'>
+                            <button className='flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg text-_main hover:text-_main/80'>
                                 <BarChart4 size={18} /> Administrar Diagnósticos
                             </button>
-                            <div className='flex gap-6 pl-6p'>
-                                <div className='px-[10px]'>
-                                    <div className='w-[2px] h-[120px] bg-_gray-border' />
+                            <div className='flex gap-2'>
+                                <div className='mx-4'>
+                                    <Separator orientation='vertical' />
                                 </div>
-                                <div className='flex flex-col gap-1 font-semibold text-_gray-C2C2C2'>
+                                <div className='flex flex-col gap-1 w-full font-semibold text-_gray-C2C2C2'>
                                     <Link
                                         href='/new'
-                                        className={clsx(`flex items-center gap-4 px-6 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select`, {
+                                        className={clsx(`flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select`, {
                                             'bg-_gray-select text-_main': pathname === '/new'
                                         })}>
                                         Nuevo Diagnóstico
                                     </Link>
                                     <Link
                                         href='/all'
-                                        className={clsx(`flex items-center gap-4 px-6 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select`, {
+                                        className={clsx(`flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select`, {
                                             'bg-_gray-select text-_main': pathname === '/all'
                                         })}>
                                         Todos los Diagnósticos
                                     </Link>
-                                    <button className='flex items-center gap-4 px-6 py-3 whitespace-nowrap rounded-lg hover:bg-_gray-select'>
+                                    <button className='flex items-center gap-4 px-4 py-3 whitespace-nowrap rounded-lg hover:bg-_gray-select'>
                                         Nuevo Diagnóstico
                                     </button>
                                 </div>
@@ -65,14 +66,14 @@ export default function Sidebar() {
                         </div>
                         <Link
                             href='/patients'
-                            className={clsx('flex items-center gap-4 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
+                            className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
                                 'text-_main': pathname === '/patients'
                             })}>
                             <Users size={18} /> Administrar Pacientes
                         </Link>
                         <Link
                             href='/terms'
-                            className={clsx('flex items-center gap-4 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
+                            className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
                                 'text-_main': pathname === '/terms'
                             })}>
                             <BadgeCheck size={18} /> Términos y Condiciones
@@ -82,12 +83,12 @@ export default function Sidebar() {
                         <div className='flex flex-col gap-2 text-_gray-808080'>
                             <Link
                                 href='/account'
-                                className={clsx('flex items-center gap-4 px-6p py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
+                                className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
                                     'text-_main': pathname === '/account'
                                 })}>
                                 <Settings size={18} /> Cuenta y Perfil
                             </Link>
-                            <button className='flex items-center gap-4 px-6p whitespace-nowrap group'>
+                            <button className='flex items-center gap-4 whitespace-nowrap group'>
                                 <div className='flex items-center gap-4 py-3 border-y border-_gray-border w-full'>
                                     <div className='w-8 h-8 rounded-full bg-_main group-hover:bg-_main/80' />
                                     <div className='flex flex-col items-start'>
