849f2057fbd71a526ee325a03c6f21b96b91a2e0
omgismartinez <martnzomg@gmail.com>
2023-07-11 22:42:22 -0600

    🚀 feat (settings): appearance page

---
+++ app/settings/appearance/page.tsx
@@ -0,0 +1,17 @@
+import { Separator } from '@/components/ui/separator'
+import { AppearanceForm } from './appearance-form'
+
+export default function Appearance() {
+    return (
+        <main>
+            <div>
+                <h1 className='text-xl font-bold'>Apariencia</h1>
+                <p className='text-_gray-808080 text-sm'>
+                    Personaliza la apariencia de la aplicación. Cambia el tema entre claro y oscuro.
+                </p>
+            </div>
+            <Separator className='my-6' />
+            <AppearanceForm />
+        </main>
+    )
+}
