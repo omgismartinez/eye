de92ccafefc22fbed95e8267d9fd1083170719bb
omgismartinez <martnzomg@gmail.com>
2023-07-11 22:23:40 -0600

    🌌 refactor (layout): settings route

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -3,24 +3,23 @@ import { Metadata } from 'next'
 import { Separator } from '@/components/ui/separator'
 import { SidebarNav } from './components/sidebar'
 
-
 export const metadata: Metadata = {
-    title: 'Cuenta y Perfil',
-    description: 'Administra tu cuenta y perfil.',
+    title: 'Configuración',
+    description: 'Administra la configuración de tu cuenta y establezca las preferencias',
 }
 
 const sidebarNavItems = [
     {
-        title: 'Cuenta',
-        href: '/account',
+        title: 'Perfil',
+        href: '/settings',
     },
     {
-        title: 'Perfil',
-        href: '/account/profile',
+        title: 'Cuenta',
+        href: '/settings/account',
     },
     {
         title: 'Apariencia',
-        href: '/account/appearance',
+        href: '/settings/appearance',
     },
 ]
 
@@ -34,11 +33,11 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
             <div className='space-y-0.5'>
                 <h2 className='text-2xl font-bold tracking-tight'>Configuración</h2>
                 <p className='text-_gray-808080 text-sm'>
-                    Administra tu perfil, apariencia y configuración de cuenta.
+                    Administrar la configuración de su cuenta y establecer las preferencias de su perfil.
                 </p>
             </div>
             <Separator className='my-6' />
-            <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
+            <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-8 lg:space-y-0'>
                 <aside className='lg:w-1/5'>
                     <SidebarNav items={sidebarNavItems} />
                 </aside>
