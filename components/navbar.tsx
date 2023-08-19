7feef37923f3726667a340468cabacc7c90f1027
omgismartinez <martnzomg@gmail.com>
2023-08-19 00:01:45 -0600

    🌌 refactor (navbar): dark theme

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,10 +18,10 @@ export default function Navbar() {
     }
 
     return (
-        <nav className='sticky top-0 z-50 bg-_white/50 backdrop-blur-sm border-b border-_gray-border'>
+        <nav className='sticky top-0 z-50 backdrop-blur-sm border-b border-_gray-border dark:border-_dark-gray'>
             <div className='flex items-center max-w-screen-2xl mx-auto overflow-hidden'>
                 <div className='flex flex-1 justify-between w-full'>
-                    <div className='lg:w-72 p-4 lg:border-r border-_gray-border'>
+                    <div className='lg:w-72 p-4 lg:border-r border-_gray-border dark:border-_dark-gray'>
                         <Avatar />
                     </div>
                     <div className='lg:hidden p-4 -mx-2'>
@@ -30,11 +30,11 @@ export default function Navbar() {
                 </div>
                 <div className='hidden lg:flex items-center lg:px-6 py-4 w-full'>
                     {pathname !== '/' && (
-                        <Link href='/' className='bg-_gray-F7F7F7 hidden lg:block rounded-full p-1'>
+                        <Link href='/' className='bg-_gray-F7F7F7 dark:bg-_dark-gray hidden lg:block rounded-full p-1'>
                             <ChevronLeft size={18} />
                         </Link>
                     )}
-                    <h2 className='text-_main font-bold text-base lg:ml-6 whitespace-nowrap'>
+                    <h2 className='text-_main dark:text-_white font-bold text-base lg:ml-6 whitespace-nowrap'>
                         {routes[pathname as keyof typeof routes]}
                     </h2>
                 </div>
