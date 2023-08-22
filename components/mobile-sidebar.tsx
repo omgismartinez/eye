e988d805b7d91b664ccbd9d78415f21f9a9c62ab
omgismartinez <martnzomg@gmail.com>
2023-08-21 22:35:52 -0600

    refactor (mobile-sidebar): supports dark mode

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -40,22 +40,21 @@ export const MobileSidebar = () => {
                     className='
                         lg:border-r
                         border-_gray-border
-                        bg-_white
                         pb-6
                         h-screen
                         overflow-auto
                     '
                 >
-                    <header className='sticky top-0 bg-_white p-4 border-b border-_gray-border'>
+                    <header className='sticky top-0 p-4 border-b border-_gray-border dark:border-_dark-gray'>
                         <Avatar />
                     </header>
-                    <h1 className='text-_main font-medium text-xs uppercase px-4 py-6'>Menu</h1>
+                    <h1 className='text-_main dark:text-_white font-medium text-xs uppercase px-4 py-6'>Menu</h1>
                     <div className='px-4'>
                         <section className='flex flex-col justify-between gap-1 min-h-[650px]'>
                             <div className='flex flex-col gap-2 text-_gray-808080 text-sm font-bold'>
                                 <div className='flex flex-col gap-4'>
-                                    <button className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
-                                        'text-_main': pathname === '/new' || pathname === '/all',
+                                    <button className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
+                                        'text-_main dark:text-_white': pathname === '/new' || pathname === '/all',
                                     })}>
                                         <BarChart4 size={18} /> Administrar Diagnósticos
                                     </button>
@@ -67,8 +66,8 @@ export const MobileSidebar = () => {
                                             <SheetClose asChild>
                                                 <Link
                                                     href='/new'
-                                                    className={clsx(`flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select`, {
-                                                        'bg-_gray-select text-_main': pathname === '/new'
+                                                    className={clsx(`flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select dark:hover:bg-_dark-gray`, {
+                                                        'bg-_gray-select text-_main dark:bg-_dark-gray dark:text-_white': pathname === '/new'
                                                     })}>
                                                     Nuevo Diagnóstico
                                                 </Link>
@@ -76,14 +75,14 @@ export const MobileSidebar = () => {
                                             <SheetClose asChild>
                                                 <Link
                                                     href='/all'
-                                                    className={clsx(`flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select`, {
-                                                        'bg-_gray-select text-_main': pathname === '/all'
+                                                    className={clsx(`flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select dark:hover:bg-_dark-gray`, {
+                                                        'bg-_gray-select text-_main dark:bg-_dark-gray dark:text-_white': pathname === '/all'
                                                     })}>
                                                     Todos los Diagnósticos
                                                 </Link>
                                             </SheetClose>
                                             <SheetClose asChild>
-                                                <button className='flex items-center gap-4 px-4 py-3 whitespace-nowrap rounded-lg hover:bg-_gray-select'>
+                                                <button className='flex items-center gap-4 px-4 py-3 whitespace-nowrap rounded-lg hover:bg-_gray-select dark:hover:bg-_dark-gray'>
                                                     Nuevo Diagnóstico
                                                 </button>
                                             </SheetClose>
@@ -93,8 +92,8 @@ export const MobileSidebar = () => {
                                 <SheetClose asChild>
                                     <Link
                                         href='/patients'
-                                        className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
-                                            'text-_main bg-_gray-select': pathname === '/patients'
+                                        className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
+                                            'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white': pathname === '/patients'
                                         })}>
                                         <Users size={18} /> Administrar Pacientes
                                     </Link>
@@ -102,8 +101,8 @@ export const MobileSidebar = () => {
                                 <SheetClose asChild>
                                     <Link
                                         href='/terms'
-                                        className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
-                                            'text-_main bg-_gray-select': pathname === '/terms'
+                                        className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
+                                            'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white': pathname === '/terms'
                                         })}>
                                         <BadgeCheck size={18} /> Términos y Condiciones
                                     </Link>
@@ -114,17 +113,17 @@ export const MobileSidebar = () => {
                                     <SheetClose asChild>
                                         <Link
                                             href='/settings'
-                                            className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80', {
-                                                'text-_main bg-_gray-select': pathname.startsWith('/settings')
+                                            className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
+                                                'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white': pathname.startsWith('/settings')
                                             })}>
                                             <Settings size={18} /> Configuración
                                         </Link>
                                     </SheetClose>
                                     <button className='flex items-center gap-4 whitespace-nowrap group'>
-                                        <div className='flex items-center gap-4 py-3 border-y border-_gray-border w-full'>
-                                            <div className='w-8 h-8 rounded-full bg-_main group-hover:bg-_main/80' />
+                                        <div className='flex items-center gap-4 py-3 border-y border-_gray-border dark:border-_dark-gray w-full'>
+                                            <div className='w-8 h-8 rounded-full bg-_main group-hover:bg-_main/80 dark:bg-white dark:group-hover:bg-_white/80' />
                                             <div className='flex flex-col items-start'>
-                                                <span className='text-xs font-bold text-_main'>Alvaro Martinez</span>
+                                                <span className='text-xs font-bold text-_main dark:text-_white'>Alvaro Martinez</span>
                                                 <span className='text-[10px] font-normal leading-tight'>Administrador</span>
                                             </div>
                                         </div>
