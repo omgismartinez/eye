52187f0e68ad05fc417c5b3d9373cf8e0c0fe9f0
omgismartinez <martnzomg@gmail.com>
2023-09-05 00:35:51 -0600

    🚀 feat (validations): auth schema

---
+++ lib/validations/auth.ts
@@ -0,0 +1,19 @@
+import * as z from 'zod'
+
+export const authSchema = z.object({
+  email: z
+    .string()
+    .email({
+      message: 'Por favor ingresa un correo electrónico válido.'
+    }),
+  password: z
+    .string()
+    .min(8, {
+      message: 'La contraseña debe tener al menos 8 caracteres.'
+    })
+    .max(100)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/, {
+      message:
+        'La contraseña debe contener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.'
+    })
+})
