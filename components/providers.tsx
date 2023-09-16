9763ac49e2e377729bc7f6b912ce7009339789f8
omgismartinez <martnzomg@gmail.com>
2023-09-15 18:43:15 -0600

    refactor (providers): db theme added

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,12 +1,18 @@
 'use client'
 
+import { useMounted } from '@/hooks/use-mounted'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
-export function ThemeProvider ({ children, ...props }: ThemeProviderProps) {
+interface Props {
+  dbTheme?: string | undefined
+}
+
+export function ThemeProvider ({ children, dbTheme, ...props }: ThemeProviderProps & Props) {
+  if (dbTheme && useMounted()) localStorage.setItem('theme', dbTheme)
   return (
-        <NextThemesProvider {...props}>
-            {children}
-        </NextThemesProvider>
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
   )
 }
