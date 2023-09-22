5e18346920dd86240e9685480442a8030cf9f71b
omgismartinez <martnzomg@gmail.com>
2023-09-21 22:15:00 -0600

    🌌 refactor (toaster): expand toaster

--- components/toaster.tsx
+++ components/toaster.tsx
@@ -5,6 +5,7 @@ import { Toaster as Sonner } from 'sonner'
 export function Toaster () {
   return (
     <Sonner
+        expand
         position='bottom-right'
         toastOptions={{
           style: {
