09bad2ef14c1e005c0ccb7a6ce86d434094c622b
omgismartinez <martnzomg@gmail.com>
2023-10-08 22:44:17 -0600

    🌌 refactor (new): diagnostic form path

--- app/(home)/(diagnostics)/new/page.tsx
+++ app/(home)/(diagnostics)/new/page.tsx
@@ -1,6 +1,6 @@
 import { type Metadata } from 'next'
 import Header from '@/components/header'
-import NewForm from './new-form'
+import { DiagnosticForm } from '@/components/forms/diagnostic-form'
 
 export const metadata: Metadata = {
   title: 'Nuevo Diagnóstico',
@@ -9,12 +9,12 @@ export const metadata: Metadata = {
 
 export default function New () {
   return (
-        <main className='max-w-3xl mx-auto'>
-            <Header
-                title='Completa los campos'
-                subtitle={<>Sube tu imagen. Arrastre y suelte <strong>1 imagen</strong> para empezar la detección.</>}
-            />
-            <NewForm />
-        </main>
+    <main className='max-w-3xl mx-auto'>
+      <Header
+        title='Completa los campos'
+        subtitle={<>Sube tu imagen. Arrastre y suelte <strong>1 imagen</strong> para empezar la detección.</>}
+      />
+      <DiagnosticForm />
+    </main>
   )
 }
