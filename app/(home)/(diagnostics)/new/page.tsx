671e5ec316985e0d58347ad668e034bc65c70385
omgismartinez <martnzomg@gmail.com>
2023-10-16 14:13:38 -0600

    🌌 refactor (diagnostic): `new diagnostic page` logic

--- app/(home)/(diagnostics)/new/page.tsx
+++ app/(home)/(diagnostics)/new/page.tsx
@@ -1,20 +1,22 @@
 import { type Metadata } from 'next'
 import Header from '@/components/header'
 import { DiagnosticForm } from '@/components/forms/diagnostic-form'
+import { currentUser } from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: 'Nuevo Diagnóstico',
   description: 'Completa los campos para realizar un nuevo diagnóstico.'
 }
 
-export default function New () {
+export default async function New () {
+  const user = await currentUser()
   return (
     <main className='max-w-3xl mx-auto'>
       <Header
         title='Completa los campos'
         subtitle={<>Sube tu imagen. Arrastre y suelte <strong>1 imagen</strong> para empezar la detección.</>}
       />
-      <DiagnosticForm />
+      <DiagnosticForm user={user} />
     </main>
   )
 }
