20205b7110dea10aa72c1ad48bc131832e2d0917
omgismartinez <martnzomg@gmail.com>
2023-10-25 20:40:28 -0600

    🌌 refactor (header): font size text whit sorted button

--- components/tables/header.tsx
+++ components/tables/header.tsx
@@ -28,54 +28,54 @@ export function DataTableColumnHeader<TData, TValue> ({
   }
 
   return (
-        <div className={cn('flex items-center space-x-2', className)}>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button
-                        variant='ghost'
-                        size='sm'
-                        className='-ml-3 h-8 data-[state=open]:bg-accent'
-                    >
-                        <span>{title}</span>
-                        {column.getIsSorted() === 'desc'
-                          ? (
-                            <SortDesc className='ml-2 h-4 w-4' />
-                            )
-                          : column.getIsSorted() === 'asc'
-                            ? (
-                            <SortAsc className='ml-2 h-4 w-4' />
-                              )
-                            : (
-                            <ChevronsUpDown className='ml-2 h-4 w-4' />
-                              )}
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align='start'>
-                    <DropdownMenuItem onClick={() => column.toggleSorting(false)}>
-                        <SortAsc className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
-                        Asc
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => column.toggleSorting(true)}>
-                        <SortDesc className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
-                        Desc
-                    </DropdownMenuItem>
-                    {column.getIsSorted() && (
-                        <DropdownMenuItem onClick={() => column.toggleSorting(undefined)}>
-                            <ChevronsUpDown className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
-                            Ninguno
-                        </DropdownMenuItem>
-                    )}
-                    {column.getCanHide() && (
-                        <>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem onClick={() => column.toggleVisibility(false)}>
-                                <EyeOff className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
-                                Ocultar
-                            </DropdownMenuItem>
-                        </>
-                    )}
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </div>
+    <div className={cn('flex items-center space-x-2', className)}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant='ghost'
+            size='sm'
+            className='-ml-3 h-8 data-[state=open]:bg-accent'
+          >
+            <span className='text-sm'>{title}</span>
+            {column.getIsSorted() === 'desc'
+              ? (
+                  <SortDesc className='ml-2 h-4 w-4' />
+                )
+              : column.getIsSorted() === 'asc'
+                ? (
+                    <SortAsc className='ml-2 h-4 w-4' />
+                  )
+                : (
+                    <ChevronsUpDown className='ml-2 h-4 w-4' />
+                  )}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='start'>
+          <DropdownMenuItem onClick={() => column.toggleSorting(false)}>
+            <SortAsc className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
+            Asc
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => column.toggleSorting(true)}>
+            <SortDesc className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
+            Desc
+          </DropdownMenuItem>
+          {column.getIsSorted() && (
+            <DropdownMenuItem onClick={() => column.toggleSorting(undefined)}>
+              <ChevronsUpDown className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
+              Ninguno
+            </DropdownMenuItem>
+          )}
+          {column.getCanHide() && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => column.toggleVisibility(false)}>
+                <EyeOff className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
+                Ocultar
+              </DropdownMenuItem>
+            </>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   )
 }
