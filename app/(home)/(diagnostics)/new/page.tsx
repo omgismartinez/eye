5f9e5e8648b5a882881f7ad83e3e12478ac0b181
omgismartinez <martnzomg@gmail.com>
2023-10-25 20:21:44 -0600

    🌌 refactor: header import

--- app/(home)/(diagnostics)/new/page.tsx
+++ app/(home)/(diagnostics)/new/page.tsx
@@ -1,5 +1,5 @@
 import { type Metadata } from 'next'
-import Header from '@/components/header'
+import { Header } from '@/components/header'
 import { DiagnosticForm } from '@/components/forms/diagnostic-form'
 import { currentUser } from '@clerk/nextjs'
 
