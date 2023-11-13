9da3a54bb9c930f279f959fe9980d5a77fb10c9c
omgismartinez <martnzomg@gmail.com>
2023-11-13 00:11:26 -0600

    refactor: add role-based access control for patients page

--- app/(home)/patients/page.tsx
+++ app/(home)/patients/page.tsx
@@ -6,6 +6,7 @@ import { columns, columnsVisibility } from './columns'
 import { Separator } from '@/components/ui/separator'
 import { getPatientsAction } from '@/app/actions/diagnostic'
 import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Pacientes Registrados',
@@ -14,6 +15,7 @@ export const metadata: Metadata = {
 
 export default async function Patients () {
   const user = await currentUser()
+  if (user?.privateMetadata.role !== 'ADMIN') redirect('/')
   const data = await getPatientsAction({ user })
   return (
     <main className='max-w-4xl mx-auto'>
