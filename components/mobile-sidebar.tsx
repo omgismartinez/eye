c1401dd6273b7b63f2957699fe6318de4eeea567
omgismartinez <martnzomg@gmail.com>
2023-11-14 00:12:29 -0600

    🌌 refactor (mobile-sidebar): better readability and maintainability.

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useMounted } from '@/hooks/use-mounted'
-import { BadgeCheck, BarChart4, Menu, Settings, Users } from 'lucide-react'
+import { Menu } from 'lucide-react'
 import type { User } from '@clerk/nextjs/server'
 
 import {
@@ -23,6 +23,7 @@ import {
   AvatarImage
 } from './ui/avatar'
 import { getRole } from '@/lib/utils'
+import { sidebarNav } from '@/config/site'
 
 export const MobileSidebar = ({ user }: { user: User | null }) => {
   const pathname = usePathname()
@@ -45,8 +46,6 @@ export const MobileSidebar = ({ user }: { user: User | null }) => {
       <SheetContent side='left' className='p-0'>
         <aside
           className='
-            lg:border-r
-            border-_gray-border
             pb-6
             h-screen
             overflow-auto
@@ -70,77 +69,114 @@ export const MobileSidebar = ({ user }: { user: User | null }) => {
           <div className='px-4'>
             <section className='flex flex-col justify-between gap-1 min-h-[650px]'>
               <div className='flex flex-col gap-2 text-_gray-808080 text-sm font-bold'>
-                <div className='flex flex-col gap-4'>
-                  <button
-                    className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
-                      'text-_main dark:text-_white': pathname === '/new' || pathname === '/all'
-                    })}>
-                    <BarChart4 size={18} /> Administrar Diagnósticos
-                  </button>
-                  <div className='flex gap-2'>
-                    <div className='mx-4'>
-                      <Separator orientation='vertical' />
-                    </div>
-                  <div className='flex flex-col gap-1 w-full font-semibold text-_gray-C2C2C2'>
-                  <SheetClose asChild>
-                    <Link
-                      href='/new'
-                      className={clsx('flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select dark:hover:bg-_dark-gray', {
-                        'bg-_gray-select text-_main dark:bg-_dark-gray dark:text-_white': pathname === '/new'
-                      })}>
-                        Nuevo Diagnóstico
-                    </Link>
-                    </SheetClose>
-                    <SheetClose asChild>
-                      <Link
-                        href='/all'
-                        className={clsx('flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select dark:hover:bg-_dark-gray', {
-                          'bg-_gray-select text-_main dark:bg-_dark-gray dark:text-_white': pathname === '/all'
-                        })}
-                      >
-                        Todos los Diagnósticos
-                      </Link>
-                    </SheetClose>
-                    <SheetClose asChild>
-                      <button className='flex items-center gap-4 px-4 py-3 whitespace-nowrap rounded-lg hover:bg-_gray-select dark:hover:bg-_dark-gray'>
-                        Nuevo Diagnóstico
-                      </button>
-                    </SheetClose>
+                {sidebarNav.top.map((route, index) => {
+                  const protectedRoute = route.children?.filter(child => child.permissions?.includes(role))
+                  const IconChildren = protectedRoute?.[0]?.icon ?? null
+                  const IconRoute = route.icon
+                  return (
+                  <div key={index}>
+                    {route.children
+                      ? protectedRoute?.length === 1 && IconChildren
+                        ? (
+                            <SheetClose asChild>
+                              <Link
+                                href={protectedRoute[0].path}
+                                className={clsx(
+                                  'flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80',
+                                  {
+                                    'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white':
+                                      pathname === protectedRoute[0].path
+                                  }
+                                )}
+                              >
+                                {<IconChildren size={20} />} {protectedRoute[0].name}
+                              </Link>
+                            </SheetClose>
+                          )
+                        : (
+                            <div className='flex flex-col gap-2'>
+                              <button
+                                className={clsx(
+                                  'flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80',
+                                  {
+                                    'text-_main dark:text-_white':
+                                      pathname === route.path
+                                  }
+                                )}
+                              >
+                                {<IconRoute size={20} />} {route.name}
+                              </button>
+                              <div className='flex gap-2'>
+                                <div className='mx-4'>
+                                  <Separator orientation='vertical' />
+                                </div>
+                                <div className='flex flex-col gap-1 w-full font-semibold text-_gray-C2C2C2'>
+                                  {route.children.map((child, index) => {
+                                    return (
+                                      <SheetClose key={index} asChild>
+                                        <Link
+                                          href={child.path}
+                                          className={clsx(
+                                            'flex items-center gap-4 px-4 py-3 w-full whitespace-nowrap rounded-lg hover:bg-_gray-select dark:hover:bg-_dark-gray',
+                                            {
+                                              'bg-_gray-select text-_main dark:bg-_dark-gray dark:text-_white':
+                                                pathname === child.path,
+                                              hidden: !child.permissions.includes(role)
+                                            }
+                                          )}
+                                        >
+                                          {child.name}
+                                        </Link>
+                                      </SheetClose>
+                                    )
+                                  })}
+                                </div>
+                              </div>
+                            </div>
+                          )
+                      : (
+                          <SheetClose asChild>
+                            <Link
+                              href={route.path}
+                              className={clsx(
+                                'flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80',
+                                {
+                                  'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white':
+                                    pathname === route.path,
+                                  hidden: route.permissions && !route.permissions.includes(role)
+                                }
+                              )}
+                            >
+                              {<IconRoute size={20} />} {route.name}
+                            </Link>
+                          </SheetClose>
+                        )}
                   </div>
-                </div>
+                  )
+                })}
               </div>
-              <SheetClose asChild>
-                <Link
-                  href='/patients'
-                  className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
-                    'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white': pathname === '/patients'
-                  })}
-                >
-                  <Users size={18} /> Administrar Pacientes
-                </Link>
-              </SheetClose>
-              <SheetClose asChild>
-                <Link
-                  href='/terms'
-                  className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
-                    'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white': pathname === '/terms'
-                  })}>
-                    <BadgeCheck size={18} /> Términos y Condiciones
-                </Link>
-              </SheetClose>
-            </div>
             <div className='flex flex-col gap-6 text-_gray-C2C2C2 text-sm font-bold'>
               <div className='flex flex-col gap-2 text-_gray-808080'>
-                <SheetClose asChild>
-                  <Link
-                    href='/settings'
-                    className={clsx('flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80', {
-                      'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white': pathname.startsWith('/settings')
-                    })}
-                  >
-                    <Settings size={18} /> Configuración
-                  </Link>
-                </SheetClose>
+                {sidebarNav.bottom.map((route, index) => {
+                  const IconRoute = route.icon
+                  return (
+                    <SheetClose key={index} asChild>
+                      <Link
+                        href={route.path}
+                        className={clsx(
+                          'flex items-center gap-4 px-2 py-3 whitespace-nowrap rounded-lg hover:text-_main/80 dark:hover:text-_white/80',
+                          {
+                            'text-_main bg-_gray-select dark:bg-_dark-gray dark:text-_white':
+                              pathname.startsWith(route.path),
+                            hidden: route.permissions && !route.permissions.includes(role)
+                          }
+                        )}
+                      >
+                        {<IconRoute size={20} />} {route.name}
+                      </Link>
+                    </SheetClose>
+                  )
+                })}
                 <button className='flex items-center gap-4 whitespace-nowrap group'>
                   <div className='flex items-center gap-4 py-3 border-y border-_gray-border dark:border-_dark-gray w-full'>
                     <Avatar className='h-8 w-8'>
@@ -151,8 +187,12 @@ export const MobileSidebar = ({ user }: { user: User | null }) => {
                       <AvatarFallback>{initials}</AvatarFallback>
                     </Avatar>
                     <div className='flex flex-col items-start'>
-                      <span className='text-xs font-bold text-_main dark:text-_white'>{user?.firstName} {user?.lastName}</span>
-                      <span className='text-[10px] font-normal leading-tight capitalize'>{role}</span>
+                      <span className='text-xs font-bold text-_main dark:text-_white'>
+                        {user?.firstName} {user?.lastName}
+                      </span>
+                      <span className='text-[10px] font-normal leading-tight capitalize'>
+                        {role}
+                      </span>
                     </div>
                   </div>
                 </button>
