4058a1879167f3e5b3bd2cdca8bae22c575decd3
omgismartinez <martnzomg@gmail.com>
2023-11-17 00:41:16 -0600

    refactor (columns): use direct actions buttons from patients

--- app/(home)/patients/columns.tsx
+++ app/(home)/patients/columns.tsx
@@ -3,7 +3,13 @@
 import { type ColumnDef } from '@tanstack/react-table'
 
 import { DataTableColumnHeader } from '@/components/tables/header'
-import { DataTableRowActions } from './actions'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
 import { Badge } from '@/components/ui/badge'
 import { Checkbox } from '@/components/ui/checkbox'
@@ -12,6 +18,9 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
 import { type PatientModel } from '@/types'
 import { type Prisma } from '@prisma/client'
+import { MoreHorizontal, Pen, Trash } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export const columnsVisibility = {
   Paciente: true,
@@ -166,6 +175,33 @@ export const columns: Array<ColumnDef<PatientModel>> = [
   },
   {
     id: 'actions',
-    cell: ({ row }) => <DataTableRowActions row={row} />
+    cell: ({ row }) => (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant='ghost'
+            className='flex h-8 w-8 p-0 data-[state=open]:bg-muted'
+          >
+            <MoreHorizontal className='h-4 w-4' />
+            <span className='sr-only'>Menú abierto</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end' className='w-[160px]'>
+          <DropdownMenuItem asChild>
+            <Link
+              href={`/patients/${row.original.id}`}
+            >
+              <Pen className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
+              Editar
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Trash className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
+            Borrar
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
   }
 ]
