bb889e57ba5f24242e8868effac3a88a90eb56b4
omgismartinez <martnzomg@gmail.com>
2023-11-29 01:00:14 -0600

    🚀 feat: add serverActions and images domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      'z39deuzvs3yujlfm.public.blob.vercel-storage.com',
+    ]
   }
 }
 
